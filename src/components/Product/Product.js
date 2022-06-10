db3eeec33d87eb18a2ca40585375ead65aaa649f
Scott Wilson <swrs@hotmail.co.uk>
2022-06-10 13:39:33 +0100

    destructured the product props and added test id

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,19 +1,21 @@
 import css from "./Product.module.css";
 
-function Product({product}) {
-    return (
-        <div className={css.product}>
-            <img
-                src={product.image}
-                alt="ProductImage"
-                className={css.productImage}
-            />
-            <h3 className={css.headline}>{product.title}</h3>
-            <p className={css.productDescription}>{product.description}</p>
-            <div className={css.categoryPrice}>
-                <p className={css.category}>{product.category}</p>
-                <p className={css.price}>{product.price}</p>
+function Product({ product }) {
+    console.log(product);
+    let title = product.title;
+    let image = product.image;
+    let description = product.description;
+    let category = product.category;
+    let price = product.price;
 
+    return (
+        <div className={css.product} testid="product">
+            <img src={image} alt="ProductImage" className={css.productImage} />
+            <h3 className={css.headline}>{title}</h3>
+            <p className={css.productDescription}>{description}</p>
+            <div className={css.categoryPrice}>
+                <p className={css.category}>{category}</p>
+                <p className={css.price}>{price}</p>
             </div>
         </div>
     );
