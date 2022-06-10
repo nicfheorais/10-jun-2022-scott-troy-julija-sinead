4273ee90c5b4b540d95ab3edcfa6d9d41d32bba1
troy <troywebb95@gmail.com>
2022-06-10 14:16:38 +0100

    fixed overflowing elements on products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,18 +4,24 @@ function Product({ product }) {
     console.log(product);
     let title = product.title;
     let image = product.image;
-    let description = product.description;
+    let description = product.description.substring(0, 100);
     let category = product.category;
     let price = product.price;
 
     return (
         <div className={css.product} testid="product">
-            <img src={image} alt="ProductImage" className={css.productImage} />
+            <div className={css.imageContainer}>
+                <img
+                    src={image}
+                    alt="ProductImage"
+                    className={css.productImage}
+                />
+            </div>
             <h3 className={css.headline}>{title}</h3>
-            <p className={css.productDescription}>{description}</p>
+            <p className={css.productDescription}>{description}...</p>
             <div className={css.categoryPrice}>
                 <p className={css.category}>{category}</p>
-                <p className={css.price}>{price}</p>
+                <p className={css.price}>£{price}</p>
             </div>
         </div>
     );
