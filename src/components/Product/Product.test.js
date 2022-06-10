a67505888c1cd4131d1f9480b0a82825f2bc9290
nicfheorais <7238289+nicfheorais@users.noreply.github.com>
2022-06-10 12:11:10 +0100

    setting up skeleton test files

---
+++ src/components/Product/Product.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Product from "./Product.js";
+
+// describe("one Product Tile displays", () => {
+//     const operatorButtons = ["+", "-", "*", "/", "="];
+
+//     test.each(operatorButtons)("renders operator button %s", (operator) => {
+//         render(<Product />);
+//         const button = screen.getByText(operator);
+//         expect(button).toBeInTheDocument();
+//     });
+// });
